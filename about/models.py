a71fde6a4bcba93e285fc3eac86d1ed467194aab
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-07-14 23:42:36 +0100

    Add cloudinary

--- about/models.py
+++ about/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 class About(models.Model):
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
+    profile_picture = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
         return self.title
