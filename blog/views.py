b1e7b0f8d26975c11874da53960de0de7921d772
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-07-14 13:53:42 +0100

    Add comments

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import CommentForm
+from django.contrib import messages
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -25,9 +27,29 @@ def post_detail(request, slug):
 
   queryset = Post.objects
   post = get_object_or_404(queryset, slug=slug)
+  comments = post.comments.all().order_by("-created_on")
+  comment_count = post.comments.filter(approved=True).count()
+  if request.method == "POST":
+    comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+        messages.add_message(
+        request, messages.SUCCESS,
+        'Comment submitted and awaiting approval'
+    )
+
+  comment_form = CommentForm()
 
   return render(
       request,
       "blog/post_detail.html",
-      {"post": post},
+      {
+          "post": post,
+          "comments": comments,
+          "comment_count": comment_count,
+          "comment_form": comment_form,
+      },
   )
